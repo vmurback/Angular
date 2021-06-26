8d061e867743416c93a6a113453f75aa7d41b45a
Vitor <vitormurback@outlook.com>
2021-06-25 23:42:35 -0300

    Implementação de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'entrar', pathMatch: 'full' },
+  { path: 'cadastrar', component: CadastrarComponent},
+  { path: 'entrar', component: EntrarComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
