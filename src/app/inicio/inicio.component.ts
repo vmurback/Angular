4f360d9491cd42337e7069f941aaeea22fc72d3c
Vitor <vitormurback@outlook.com>
2021-07-01 01:30:14 -0300

    Componente inicio sem consumo

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  constructor(
 
-  ngOnInit(): void {
+    private router: Router
+  ) { 
+
+
+}
+
+
+ngOnInit(){
+
+  // if (environment.token == '') {
+
+  //   alert('Sua seção expirou, faça o login novamente.')
+  //   this.router.navigate(['/entrar'])
+  //   }
   }
-
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
 
@@ -11,12 +12,22 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
-  entrar(userLogin: UserLogin): Observable<UserLogin>{
-return this.http.post<UserLogin>('https://vferminbp.herokuapp.com/usuarios/logar', userLogin)
+  entrar(userLogin: UserLogin): Observable<UserLogin> {
+    return this.http.post<UserLogin>('https://vferminbp.herokuapp.com/usuarios/logar', userLogin)
   }
 
-  cadastrar(user: User): Observable<User>{
+  cadastrar(user: User): Observable<User> {
     return this.http.post<User>('https://vferminbp.herokuapp.com/usuarios/cadastrar', user)
 
   }
+
+  logado() {
+    let ok: boolean = false
+
+    if (environment.token != ''){
+    ok = true
+  }
+    return ok
+  }
+
 }
